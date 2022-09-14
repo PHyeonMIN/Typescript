0912c13837ddf8da56e6912e1d9379a0a160ac8c
박현민 <dndb4902@naver.com>
2022-09-14 14:18:59 +0900

    # callback 함수

--- 1.TypeScript-basic/app.ts
+++ 1.TypeScript-basic/app.ts
@@ -6,6 +6,12 @@ const printResult = (num:number) => {   // void : 이 함수에 의도적으로 
     console.log('Result: '+num);
 }
 
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
+    const result = n1+n2;
+    const value = cb(result);
+}
+
+
 printResult(add(5,12));
 
 let combineValues: (a:number, b:number) => number;
@@ -13,3 +19,7 @@ combineValues = add;
 // combineValues = 5;
 // combineValues = printResult();
 console.log(combineValues(8,8));
+
+addAndHandle(10,20,(result) => {
+    console.log(result);
+});
