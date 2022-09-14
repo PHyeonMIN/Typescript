2b724f597004f93e6a95c1d5bd211246cc79d209
박현민 <dndb4902@naver.com>
2022-09-14 10:45:45 +0900

    # 튜플

--- 1.TypeScript-basic/app.ts
+++ 1.TypeScript-basic/app.ts
@@ -1,9 +1,30 @@
+// const person: {
+//     name: string,
+//     age: number
+// } = {
 const person: {
-    name: string,
-    age: number
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
 } = {
     name: 'Maximilian',
-    age:30
+    age:30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2,'author']
 };
 
+// person.role.push('admin');  // 푸시는 예외적으로 튜플에서 허용
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user']; // 재선언은 에러
+
+let favoriteActivities: any[];
+favoriteActivities = ['Sports',1];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map());    // !!! ERROR !!!
+}
