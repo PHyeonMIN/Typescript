fc6bed470c022e0b9397f0849a36bc700a56263e
박현민 <dndb4902@naver.com>
2022-09-14 11:25:22 +0900

    # Enum

--- 1.TypeScript-basic/app.ts
+++ 1.TypeScript-basic/app.ts
@@ -2,16 +2,25 @@
 //     name: string,
 //     age: number
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Maximilian',
+//     age:30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2,'author']
+// };
+
+enum Role { ADMIN='ADMIN', READ_ONLY=100, AUTHOR=200};
+
+const person = {
     name: 'Maximilian',
     age:30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2,'author']
+    role: Role.ADMIN
 };
 
 // person.role.push('admin');  // 푸시는 예외적으로 튜플에서 허용
