751351772c6ae20f6206be872289bf71493eed52
박현민 <dndb4902@naver.com>
2022-09-14 14:38:57 +0900

    # 타입 never

--- 1.TypeScript-basic/app.ts
+++ 1.TypeScript-basic/app.ts
@@ -7,3 +7,9 @@ if(typeof userInput === 'string'){
     userName = userInput;
 }
 // userName = userInput;   // userInput에 현재 저장된 타입을 확인해야함
+
+const generateError = (message: string, code: number): never => {
+    throw {message: message, errorCode:code};
+};
+
+generateError('An error occurred!',500);
