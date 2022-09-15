cbd8a5c70ff6ab7a49ff0f3895720169b2513196
박현민 <dndb4902@naver.com>
2022-09-15 14:34:14 +0900

    # public, private

--- 3.class&interface/src/app.ts
+++ 3.class&interface/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -8,10 +9,26 @@ class Department {
     describe(this: Department) {
         console.log('Department: '+ this.name);
     }
+
+    addEmployee(employee: string){
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
-accounting.describe();
 
-const accountingCopy = { name:'s', describe: accounting.describe }    // 클래스를 기반으로 하지않고 더미객체로서 생성
-accountingCopy.describe();  // undefined
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name:'s', describe: accounting.describe }    // 클래스를 기반으로 하지않고 더미객체로서 생성
+// accountingCopy.describe();  // undefined
