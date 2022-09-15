283b4d56ffa477bdb4ffa12f40ad0cb7e93403f0
박현민 <dndb4902@naver.com>
2022-09-15 15:17:22 +0900

    # protected

--- 3.class&interface/src/app.ts
+++ 3.class&interface/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
     // private readonly id: string;     // readonly : 특정 속성이 초기화되고나면 이후에는 변경 X
     // private name: string;
-    private employees: string[] = [];
+    protected employees: string[] = []; // protected : private와 다른 점은 이 클래스에서뿐만 아니라 이 클래스를 확장하는 모든 클래스에서 사용가능
 
     constructor(private readonly id: string, public name: string) {
         // this.id = id;
@@ -36,6 +36,13 @@ class AccountingDepartment extends Department {
         super(id, 'Accounting');
     }
 
+    addEmployee(name: string) {
+        if(name==='Max'){
+            return;
+        }
+        this.employees.push(name);
+    }
+
     addReport(text: string){
         this.reports.push(text);
     }
@@ -61,7 +68,10 @@ console.log(it);
 
 const accounting = new AccountingDepartment('d2',[]);
 accounting.addReport('Something went wrong...');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { name:'s', describe: accounting.describe }    // 클래스를 기반으로 하지않고 더미객체로서 생성
 // accountingCopy.describe();  // undefined
