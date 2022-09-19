7cefd48823395f9590e39a8544ff8bc35cf2ab80
박현민 <dndb4902@naver.com>
2022-09-19 14:20:10 +0900

    # 다른 일반함수

--- 5.generics/src/app.ts
+++ 5.generics/src/app.ts
@@ -17,3 +17,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge<{name: string, hobbies: string[]}, {age:number}>({name: 'Max', hobbies: ['Sports']},{age: 30});
 console.log(mergedObj.age);
+
+interface Lengthy { // length 속성이 없으면 에러
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1){
+        descriptionText = 'Got 1 elements';
+    }else if(element.length > 1){
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
